d8bfcee6c444b5016cc9603a6d02e3fbd0f3d70c
Hassaan Zeeshan <hassaanzeeshan35@gmail.com>
2026-04-28 19:37:33 +0500

    Vendors completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Rishtay from "./components/Rishtay";
 import AboutUS from "./components/AboutUS";
 import F_rishtay from "./components/F_rishtay";
 import ContactUs from "./components/ContactUs"
+import Vendors from "./components/Vendors";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Route path="/rishtay" element={<Rishtay />} />
       <Route path="/f_rishtay" element={<F_rishtay />} />
       <Route path="/contactUs" element={<ContactUs />} />
+      <Route path="/vendors" element={<Vendors />} />
       <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
   );
